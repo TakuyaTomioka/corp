dd5046a2c01e99022bae5470ac312e694edd5a45
TakuyaTomioka <taku.nowet@gmail.com>
2021-01-08 19:28:55 +0900

    Add google fonts

--- components/Layout.js
+++ components/Layout.js
@@ -8,6 +8,8 @@ const Layout = (props) => {
     <>
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@500;700&family=Noto+Sans+JP&display=swap" rel="stylesheet" />
       </Head>
       <Nav />
       {props.children}
