64d45871685e5c33667a4a20e92af1c7187af8c3
TakuyaTomioka <taku.nowet@gmail.com>
2021-01-08 12:49:59 +0900

    Add head tag

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,14 @@
-import { Children } from 'react';
+// Components
+import Head from 'next/head';
 import Nav from './Nav';
 import Footer from './Footer';
 
 const Layout = (props) => {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Nav />
       {props.children}
       <Footer />
