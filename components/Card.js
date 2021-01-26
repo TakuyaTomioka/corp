7276da2cef2cf112cd9f99a2976c4d513d6a467e
TakuyaTomioka <taku.nowet@gmail.com>
2021-01-26 15:15:09 +0900

    Create Card components

---
+++ components/Card.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
+const Card = () => {
+  return (
+    <>
+      <p>Card</p>
+    </>
+  )
+}
+
+
+export default Card;
