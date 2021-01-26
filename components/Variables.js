a668b4e6c02de98acdf4a819a7d372fbea8c8998
TakuyaTomioka <taku.nowet@gmail.com>
2021-01-26 15:13:11 +0900

    Create shared variables

---
+++ components/Variables.js
@@ -0,0 +1,19 @@
+export const color = {
+  main: '#FF4040',
+  mainLighter: '#FF7F51',
+  mainLightest: '#6FFCF',
+  sub: '#AAF0D2',
+  subDarker: '#70DAB4',
+  subDarkest: '#3EB488',
+  text: '#333',
+};
+
+export const font = {
+  xs: '10px',
+  sm: '18px',
+  base: '24px',
+  lg: '36px',
+  xl: '48px',
+  xl2: '64px',
+  xl3: '72px',
+}
