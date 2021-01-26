cbe820bee33a38de6ee64345d9a201bb0bad8aa8
TakuyaTomioka <taku.nowet@gmail.com>
2021-01-26 15:13:32 +0900

    Add main vue

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 import Head from 'next/head';
 // variables
 import { webTitle } from '../util';
+// Styled
+import { color, font } from '../components/Variables';
+import { device } from '../components/MediaQuery';
+import styled from 'styled-components';
+import { motion, AnimatePresence, AnimateSharedLayout } from "framer-motion";
 
 export default function Home() {
   return (
@@ -8,9 +13,35 @@ export default function Home() {
       <Head>
         <title>{webTitle}トップページ</title>
       </Head>
-      <main>
-        <h1>Top Page</h1>
-      </main>
+      <Main>
+        <h1>明日の<span>Web</span>を<br />
+        もう<span>ちょっと</span>よくする</h1>
+        <img src="mv.jpg" alt="" />
+      </Main>
     </>
   )
 }
+
+const Main = styled(motion.main)`
+  position: relative;
+  h1{
+    color: white;
+    font-size: ${font.lg};
+    position: absolute;
+    span{
+      color: ${color.main};
+    }
+    @media ${device.tablet}{
+      font-size: ${font.xl};
+    }
+    @media ${device.laptopL}{
+      font-size: ${font.xl2};
+      bottom: 300px;
+      left: 150px;
+      letter-spacing: 15px;
+    }
+  }
+  img{
+    width: 100%;
+  }
+`;
