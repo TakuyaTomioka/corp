cca1d20d76f855bfe3f57c1708c1696a3dd0185f
TakuyaTomioka <taku.nowet@gmail.com>
2021-01-29 13:00:11 +0900

    Add service URL

--- api.js
+++ api.js
@@ -1,1 +1,5 @@
-const baseURL = 'https://corp.microcms.io/api/v1/';
+const base_url = 'https://corp.microcms.io/api/v1/';
+
+const all_service = 'service';
+
+export const serviceURL = () => `${base_url}${all_service}`;
